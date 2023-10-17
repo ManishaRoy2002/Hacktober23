349782dc5b30f8ffd8f4f706498decee3f8cb314
obosky <obococonut@gmail.com>
2023-10-17 06:22:52 -0700

    Hacking Wizards

--- gitfetch.js
+++ gitfetch.js
@@ -56,6 +56,7 @@ const profiles = [
     {githubUsername:'DomeT99', linkedinID: 'domenico-tenace'},
     {githubUsername:'adityagupta19', linkedinID: 'adityagupta11219'},
     {githubUsername:'Ananyasingh2002', linkedinID: 'ananya-singh-29b304224'},
+    {githubUsername:'obosky', linkedinID: 'supriadi-supriadi-0a3b2627a'},
     
     // Add more profiles as needed
 ];
